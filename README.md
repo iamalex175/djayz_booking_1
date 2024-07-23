99a59b18f99fcf2ad0f6078dc927aa4f0bd1f519
Alex <kiseljowalex@gmail.com>
2024-07-23 14:45:43 +0200

    djayz

---
+++ README.md
@@ -0,0 +1,3 @@
+# djayz_booking_1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/djayz_booking_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.djayz_booking_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
